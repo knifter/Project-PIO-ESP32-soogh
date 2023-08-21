4ba854a16baddf029f8174f14297549133e876a2
K. van Nieuwland <k.vannieuwland@uva.nl>
2023-08-21 14:43:44 +0200

    Improved def_version.py to work when project is not in a git repo

--- FW/ProjectName/def_version.py
+++ FW/ProjectName/def_version.py
@@ -1,22 +1,33 @@
 import subprocess
 import datetime
+from sys import stderr
 
 #(part of) GIT revision
-revision = (
-    subprocess.check_output(["git", "rev-parse", "HEAD"])
-    .strip()
-    .decode("utf-8")
-)
-print(f'-DGIT_REVISION="{revision[-8:]}"');
+try:
+    revision = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"])
+        .strip()
+        .decode("utf-8")
+    )[-8:]
+except subprocess.CalledProcessError:
+    revision = "unknownrevision"
+    print("Warning: Could not get git commit", file=stderr)
 
-branch = (
-    subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-    .strip()
-    .decode("utf-8")
-)
-print(f'-DGIT_BRANCH="{branch}"');
+try:
+    branch = (
+        subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        .strip()
+        .decode("utf-8")
+    )
+except subprocess.CalledProcessError:
+    print("Warning: Could not get git branch", file=stderr)
+    branch = "unknownbranch"
+    
 
 #date
 curr_date = datetime.datetime.now()
 datetime = "%02d%02d%02d" % (curr_date.year, curr_date.month, curr_date.day)
+
 print(f'-DBUILD_DATETIME="{datetime}"');
+print(f'-DGIT_REVISION="{revision}"');
+print(f'-DGIT_BRANCH="{branch}"');
